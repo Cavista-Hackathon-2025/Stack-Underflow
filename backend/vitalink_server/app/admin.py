cc00f936c4b691c83fcb85a71699d21fe2322154
Eneji Ohieku <enejiohieku@gmail.com>
2025-02-22 14:28:30 +0100

    regular sync

--- backend/vitalink_server/app/admin.py
+++ backend/vitalink_server/app/admin.py
@@ -7,7 +7,9 @@ admin.site.site_header = 'Vitalink Admin'                    # default: "Django 
 admin.site.index_title = 'Monitor Prev Data'                 # default: "Site administration"
 admin.site.site_title = 'Vitalink history data monitor'                  # default: "Django site admin"
 
+admin.site.register(Config)
+
 @admin.register(VitalinkRecord)
 class VitalinkRecordAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
-        return ["timestamp", "temp", "spo2", "bpm", "sbp", "dbp"]
+        return ["timestamp", "age", "gender", "temp", "spo2", "bpm", "sbp", "dbp"]
